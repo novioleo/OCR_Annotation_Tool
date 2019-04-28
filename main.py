6669cc19245a08e6ff01863235b6021db204c323
novioleo <744351893@qq.com>
2019-04-28 17:00:23 +0800

    实现用canvas进行图片浏览，并能够在图片上画点

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
 from glob import glob
+from tkinter import *
 from tkinter import filedialog
 
 from PIL import ImageTk, Image
@@ -13,20 +14,18 @@ class Application(tk.Frame):
         self.index = 0
         self.master = master
         tk.Frame.__init__(self, master)
+        self.canvas = tk.Canvas(self, height=300, width=300)
+        self.canvas.pack()
+        self.img = None
         self.pack()
-        self.init_widget()
         self.init_bind()
         self.init_image_folder()
-
-    def init_widget(self):
-        # 初始化照片区域
-        self.label_image = tk.Label(self)
-        self.label_image.pack()
 
     def init_bind(self):
         # 绑定方向键
         self.master.bind('<Left>', lambda event: self.prev())
         self.master.bind('<Right>', lambda event: self.next())
+        self.canvas.bind('<Button-1>', self.draw_point)
 
     def init_image_folder(self):
         # 只加载jpg和png的图片
@@ -34,24 +33,31 @@ class Application(tk.Frame):
             filter(lambda x: os.path.splitext(x)[1].lower() in {'.jpg', '.png'},
                    glob(os.path.join(filedialog.askdirectory(), '*'))))
         self.size_of_images = len(self.image_files)
-        self.showfile(self.index)
+        self.show_file(self.index)
 
     def prev(self):
         if self.image_files:
-            self.showfile(-1)
+            self.show_file(-1)
 
     def next(self):
         if self.image_files:
-            self.showfile(1)
+            self.show_file(1)
 
-    def showfile(self, n):
+    def draw_point(self, event, r=5):
+        self.canvas.create_oval(event.x - r, event.y - r, event.x + r, event.y + r,fill='red',outline='red')
+
+    def show_file(self, n):
         self.index += n
         if self.index < 0:
             self.index = self.size_of_images - 1
         if self.index > (self.size_of_images - 1):
             self.index = 0
         self.img = ImageTk.PhotoImage(Image.open(self.image_files[self.index]))
-        self.label_image['image'] = self.img
+        width, height = self.img.width(), self.img.height()
+        self.canvas.delete('all')
+        self.canvas.config(width=width, height=height)
+        self.canvas.create_image(0, 0, anchor=NW, image=self.img, tag='background')
+        self.canvas.update()
 
 
 root = tk.Tk()
